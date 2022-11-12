6caec42775f1b4b6b0670b27df990621707cf5a8
MuzafarovAA <artmuz@mail.ru>
2022-11-12 13:58:11 +0500

    interfaces, factory added

---
+++ src/main/java/ru/artmuz/factories/SorterFactory.java
@@ -0,0 +1,27 @@
+package ru.artmuz.factories;
+
+import ru.artmuz.entities.CommonResult;
+import ru.artmuz.interfaces.ISorter;
+import ru.artmuz.interfaces.ISorterFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SorterFactory implements ISorterFactory {
+
+    private final List<ISorter> sorters = new ArrayList<>();
+
+    @Override
+    public Map<ISorter, Integer> doSort(int[] array) {
+        Map<ISorter, Integer> hashMap = new HashMap<>();
+
+        for (ISorter sorter : sorters) {
+            CommonResult result = sorter.doSort(array);
+            hashMap.put(sorter, result.getCount());
+        }
+
+        return hashMap;
+    }
+}
